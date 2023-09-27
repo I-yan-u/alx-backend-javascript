442cb1ea997ac435408fc9761153e398dc221bb8
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-09-27 20:30:52 +0100

    What comes after the promise?

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,6 +2,6 @@ export default function handleResponseFromAPI(promise) {
   const res = { status: 200, body: 'Success' };
   return promise
     .then(() => res)
-    .catch(() => new Error())
+    .catch(() => { console.log(new Error()); })
     .finally(() => { console.log('Got a response from the API'); });
 }
