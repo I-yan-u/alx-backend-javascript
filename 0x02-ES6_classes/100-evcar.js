c494e98b07938df7b777bd30ac51627a2433493e
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-02 00:12:49 +0100

    evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+/* eslint no-underscore-dangle: ["error", {"allow": ["_brand", "_motor", "_color", "_range"] }] */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // get [Symbol.species]() {
+  //   return Car;
+  // }
+
+  cloneCar() {
+    const Obj = super.constructor || super[Symbol.species];
+    return new Obj();
+  }
+}
