9843141406a22e237a4372718de01f0a705cd253
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-04 02:19:41 +0100

    updated if statement -line:5

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startstr) {
   const result = [];
   if (startstr === '' || typeof startstr !== 'string') return '';
   set.forEach((element) => {
-    if (element.startsWith(startstr)) {
+    if (typeof element === 'string' && element.startsWith(startstr)) {
       result.push(element.slice(startstr.length));
     }
   });
